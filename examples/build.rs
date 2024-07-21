a8e6da8deed82dcb8e0bcdf6b65aa92cf8388a07
Jose Biehl <osobiehl@gmail.com>
2024-07-21 17:44:30 +0200

    wip

--- examples/build.rs
+++ examples/build.rs
@@ -48,4 +48,5 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    println!("cargo:rustc-link-arg-bins=-Map=map.map");
 }
